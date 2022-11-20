a0003001f3270a7e6a01d2cad275045fe8cdd275
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-11-20 15:59:44 -0500

    added api connection

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 document.querySelector("button").addEventListener("click", getDrink)
-
+const key = config.secret;
 function getDrink () {
   let date = document.querySelector("input").value;
 
-  fetch(`https://api.nasa.gov/planetary/apod`)
+  fetch(`https://api.nasa.gov/planetary/apod${key}`)
                     /*Gets query value from date input*/
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
-      console.log(data.drinks);
-      document.querySelector("img").src = data.drinks[0].strDrinkThumb;
-      document.querySelector("h2").innerText = data.drinks[0].strDrink;
-      document.querySelector("h3").innerText = data.drinks[0].strInstructions;
+      console.log(data);
+
+      // document.querySelector("img").src = data.drinks[0].strDrinkThumb;
+      // document.querySelector("h2").innerText = data.drinks[0].strDrink;
+      // document.querySelector("h3").innerText = data.drinks[0].strInstructions;
+
     })
     .catch((err) => {
       console.log(`error ${err}`);
