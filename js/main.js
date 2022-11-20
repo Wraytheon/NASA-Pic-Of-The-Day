25203d72cdc2b18d55198bfc8f8091cae68873c8
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-11-20 16:44:40 -0500

    full basic functionality achieved

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-document.querySelector("button").addEventListener("click", getDrink)
+document.querySelector("button").addEventListener("click", getFetch)
+// document.addEventListener("enter", getFetch)
 const key = config.secret;
-function getDrink () {
+function getFetch () {
   const dateInput = document.querySelector("input").value;
   let dateQuery = "&date=" + dateInput
 
@@ -9,19 +10,20 @@ function getDrink () {
                     /*Gets query value from date input*/
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
+      //!video vs image logic
+      format = data.media_type;
+      if (format === "video") {
+        document.querySelector("embed").src = data.url;
+      } else if (format === "image"){
+        document.querySelector("img").src = data.url;
+      }
       console.log(data);
       document.querySelector("h2").innerText = data.title;
       document.querySelector("h3").innerText = data.explanation;
-
-      // document.querySelector("img").src = data.drinks[0].strDrinkThumb;
-      // document.querySelector("h2").innerText = data.drinks[0].strDrink;
-      // document.querySelector("h3").innerText = data.drinks[0].strInstructions;
 
     })
     .catch((err) => {
       console.log(`error ${err}`);
     });
 
-    //!video vs image logic
-    //?Vids only have url, not hdurl
 }
