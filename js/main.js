35d87b01d199edac6645df89a7146f3c99b602ba
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-11-20 16:28:27 -0500

    description and titles working

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 document.querySelector("button").addEventListener("click", getDrink)
 const key = config.secret;
 function getDrink () {
-  let date = document.querySelector("input").value;
+  const dateInput = document.querySelector("input").value;
+  let dateQuery = "&date=" + dateInput
 
-  fetch(`https://api.nasa.gov/planetary/apod${key}`)
+
+  fetch(`https://api.nasa.gov/planetary/apod${key}${dateQuery}`)
                     /*Gets query value from date input*/
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data);
+      document.querySelector("h2").innerText = data.title;
+      document.querySelector("h3").innerText = data.explanation;
 
       // document.querySelector("img").src = data.drinks[0].strDrinkThumb;
       // document.querySelector("h2").innerText = data.drinks[0].strDrink;
@@ -17,4 +21,7 @@ function getDrink () {
     .catch((err) => {
       console.log(`error ${err}`);
     });
+
+    //!video vs image logic
+    //?Vids only have url, not hdurl
 }
